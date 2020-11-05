de7ff5bcf74493d4b2fbddb066d6ac37fda5a015
rebel-l <dj@rebel-l.net>
2020-11-05 23:39:34 +0100

    #17: added animation for cancel timeout

--- src/components/shop/list-element/ListElement.js
+++ src/components/shop/list-element/ListElement.js
@@ -3,8 +3,8 @@
 import styles from './ListElement.scss';
 import React, { Component } from 'react';
 
-const cancelTimeout = 10000;
-const cancelTimer = 100; // TODO: better naming
+const cancelTimeout = 2000; // ms
+const progressInterval = 10; // ms
 
 class ListElement extends Component {
     constructor(props) {
@@ -13,24 +13,32 @@ class ListElement extends Component {
         // init attributes
         this.timer = null;
 
+        // init state
+        this.state = {
+            progress: 0 + '%'
+        }
+
         // register event handler
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(){
         if(this.timer === null) {
-            let maxIterations = cancelTimeout / cancelTimer;
+            let iterations = cancelTimeout / progressInterval;
+            let step = 100 / iterations;
             let counter = 0;
             let self = this;
             this.timer = setInterval(function (){
-                if(counter < maxIterations){
-                    console.log('animate', counter * cancelTimer);
-                    counter++;
+                counter++;
+                let progress = counter * step;
+
+                if(counter < iterations){
+                    self.setProgress(progress);
                 } else {
                     self.resetTimer();
                     console.log('timed out');
                 }
-            }, cancelTimer);
+            }, progressInterval);
         } else {
             console.log('action canceled');
             this.resetTimer();
@@ -38,14 +46,19 @@ class ListElement extends Component {
     }
 
     resetTimer(){
+        this.setProgress(0);
         clearInterval(this.timer);
         this.timer = null;
     }
 
+    setProgress(value){
+        this.setState({progress: value + '%'});
+    }
+
     render() {
         return (
-            <div className={styles.listElementOuter}>
-                <div className={styles.listElement} onClick={this.handleClick}>ELEMENT</div>
+            <div className={styles.listElementOuter} onClick={this.handleClick}>
+                <div className={styles.listElementInner} style={{width: this.state.progress}}>ELEMENT</div>
             </div>
         );
     }
