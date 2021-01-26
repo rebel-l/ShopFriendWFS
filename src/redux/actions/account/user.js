acece79c64c0052b38d7da2453feb9d168492b75
Lars Gaubisch <l.gaubisch@googlemail.com>
2021-01-27 00:03:13 +0100

    #5: change API middleware to a common approach

--- src/redux/actions/account/user.js
+++ src/redux/actions/account/user.js
@@ -1,13 +1,32 @@
 import {API} from "../../types/api";
 import {ACCOUNT_USER_LOGGED_IN} from "../../types/account/user";
 
+/**
+ * Triggers the XHR call to login with facebook.
+ *
+ * @param accessToken
+ * @returns {{payload: {path: string, method: string, service: string}, type: string}}
+ */
 export function loginFacebook(accessToken) {
     return {
         type: API,
-        payload: accessToken
-    };
+        payload: {
+            method: 'PUT',
+            service: 'auth',
+            path: '/public/facebook/login',
+            data: {
+                'AccessToken': accessToken
+            }
+        }
+    }
 }
 
+/**
+ * Triggered as soon as the user was logged in successfully.
+ *
+ * @param user
+ * @returns {{payload, type: string}}
+ */
 export function loggedIn(user) {
     return {
         type: ACCOUNT_USER_LOGGED_IN,
