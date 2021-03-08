5c0b19abc1637af1f92f546c65fca5f064abebe9
Lars Gaubisch <l.gaubisch@googlemail.com>
2021-03-08 22:43:21 +0100

    #12: fixes regarding linters

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
+import { deleteNotifications } from "../redux/actions/notification";
+import { ERROR } from "../model/notification";
+import PropTypes from "prop-types";
+
 import styles from "./Notification.scss";
 
-import { ERROR } from "../model/notification";
-import { deleteNotifications } from "../redux/actions/notification";
-
-const mapStateToProps = (state) => ({ "notifications": state.notification });
+const
+    buttonLabelClose = "close",
+    emptyNotifications = 0,
+    mapStateToProps = (state) => ({ "notifications": state.notification });
 
 /**
  * Notification is the UI component to display notifications to the user.
@@ -18,27 +23,50 @@ class Notification extends Component {
         this.handleClose = this.handleClose.bind(this);
     }
 
+    shouldComponentUpdate (nextProps) {
+        const { notifications } = this.props;
+
+        if (notifications.length !== nextProps.notifications.length) {
+            return true;
+        }
+
+        for (let i = 0; i < notifications.length; i++) {
+            const current = notifications[i],
+                next = nextProps[i];
+
+            if (next.type !== current.type || next.message !== current.message) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Closes the notifications by deleting it from the redux store.
      */
     handleClose () {
+        // eslint-disable-next-line react/destructuring-assignment
         this.props.deleteNotifications();
     }
 
     render () {
-        if (this.props.notifications.length === 0) {
+        const { notifications } = this.props;
+
+        if (notifications.length === emptyNotifications) {
             return "";
         }
 
         return (
             <div className={styles.notification}>
                 {
-                    this.props.notifications.map((item, index) => {
+                    notifications.map((item, index) => {
                         const style = item.type === ERROR ? styles.error : "";
 
                         return (
                             <div className={style}
                                 id={index}
+                                key={index}
                             >
                                 {item.message}
                             </div>
@@ -47,12 +75,17 @@ class Notification extends Component {
                 }
                 <div className={styles.last}>
                     <button onClick={this.handleClose}>
-                        close
+                        {buttonLabelClose}
                     </button>
                 </div>
             </div>
         );
     }
 }
+
+Notification.propTypes = {
+    "deleteNotifications": PropTypes.func.isRequired,
+    "notifications": PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps, { deleteNotifications })(Notification);
