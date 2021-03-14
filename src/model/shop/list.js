2b0377101e69761bd6bf6f1014b603f9d4fe3ac2
Lars Gaubisch <l.gaubisch@googlemail.com>
2021-03-14 22:41:17 +0100

    #12: fixes regarding linters

--- src/model/shop/list.js
+++ src/model/shop/list.js
@@ -1,42 +1,42 @@
 function NewList (items) {
     return {
         items,
-        sort,
-        swapItems,
-    };
-}
 
-/**
- * Sort orders the items.
- * Default order is by item.active
- */
-function sort () {
-    for (let i = 0; i < this.items.length - 1; i++) {
-        if (!this.items[i].active && this.items[i + 1].active) {
-            this.swapItems(i, i + 1);
+        /**
+         * Sort orders the items.
+         * Default order is by item.active
+         */
+        sort () {
+            /* eslint-disable  no-magic-numbers */
+            for (let i = 0; i < this.items.length - 1; i++) {
+                if (!this.items[i].active && this.items[i + 1].active) {
+                    this.swapItems(i, i + 1);
 
-            for (let j = i; j > 1; j--) {
-                if (!this.items[j].active && this.items[j - 1].active) {
-                    break;
+                    // eslint-disable-next-line id-length
+                    for (let j = i; j > 1; j--) {
+                        if (!this.items[j].active && this.items[j - 1].active) {
+                            break;
+                        }
+
+                        this.swapItems(j - 1, j);
+                    }
                 }
-
-                this.swapItems(j - 1, j);
             }
-        }
-    }
-}
+        },
 
-/**
- * SwapItems swap the item with index a & b.
- *
- * @param a {number}
- * @param b {number}
- */
-function swapItems (a, b) {
-    const temp = this.items[a];
+        /**
+         * SwapItems swap the item with index a & b.
+         *
+         * @param first {number}
+         * @param second {number}
+         */
+        swapItems (first, second) {
+            const temp = this.items[first];
 
-    this.items[a] = this.items[b];
-    this.items[b] = temp;
+            this.items[first] = this.items[second];
+            this.items[second] = temp;
+        },
+    };
 }
 
 export default NewList;
